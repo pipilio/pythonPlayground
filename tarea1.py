293e5c011dac6ad47b8bca88410864a30d71fdc6
MetsuPipilio <alonsohahn@gmail.com>
2021-02-17 17:56:04 -0300

    mi respuesta

--- tarea1.py
+++ tarea1.py
@@ -1,6 +1,14 @@
 # esta función debe contar la cantidad de vocales que tiene palabra
 def cuentaVocales(palabra):
-    return 0
+    loweredPalabra = palabra.lower
+    vocales = 0
+    for i in range(0, len(loweredPalabra)):
+        if esVocal(loweredPalabra[i]) == True:
+            vocales = vocales + 1
+    return vocales
 
 def esVocal(letra):
     return "aeiou".find(letra) != -1
+
+Palabras = cuentaVocales("paralelepipedo")
+print(Palabras)
